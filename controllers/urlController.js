f48fe511daed672b3287a1a0d1c95e533b77d0f2
Shubham Maske <shubhammaske099@gmail.com>
2025-02-09 13:00:12 +0530

    added controller logic for redirectUrl from short url and store the analytics of url

--- controllers/urlController.js
+++ controllers/urlController.js
@@ -2,6 +2,8 @@ const Url = require('../models/Url');
 const redisClient = require('../config/redisClient');
 const nanoid = require('nanoid');
 const isEmpty = require('../utils/isEmpty')
+const useragent = require('useragent');
+const geoip = require('geoip-lite');
 
 exports.createShortUrl = async (req, res) => {
   try {
@@ -24,7 +26,45 @@ exports.createShortUrl = async (req, res) => {
 
 exports.redirectUrl = async (req, res) => {
     try {
-      
+      const { alias } = req.params;
+      if(isEmpty(alias)) {
+        return res.status(403).json({ message: 'Input url should not be empty' });
+      }
+      let longUrl = await redisClient.get(alias);
+      if (!longUrl) {
+        const url = await Url.findOne({ shortUrl: alias });
+        if (!url) {
+          return res.status(404).json({ message: 'URL not found' });
+        }
+
+        const agent = useragent.parse(req.headers['user-agent']);
+        const osName = agent.os.toString();
+        const deviceType = agent.device.family.toLowerCase().includes('mobile') ? 'mobile' : 'desktop';
+
+        // Getting the userIP and user-agent
+        const userAgent = req.headers['user-agent'];
+        const ip = req.ip;
+
+        const geo = geoip.lookup(ip) || {};
+        const geolocation = {
+          country: geo.country || 'Unknown',
+          region: geo.region || 'Unknown',
+          city: geo.city || 'Unknown',
+        };
+
+        url.analytics.push({ 
+          userAgent, 
+          ip, 
+          geolocation, 
+          osName,
+          deviceType, 
+        });
+        await url.save();
+
+        longUrl = url.longUrl;
+        await redisClient.setEx(alias, 3600, longUrl);
+      }
+      res.redirect(longUrl);
     } catch (error) {
       console.log("redirectUrl error - ",error)
       res.status(500).json({ message: 'Error redirecting URL', error });
